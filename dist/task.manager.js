9042ebfefb1d354542d9d8ce5c3a9a47716c53a3
Tony Pernicano <tony@ensignemblem.com>
2016-06-24 18:24:53 -0400

    Starts task manager.

---
+++ dist/task.manager.js
@@ -0,0 +1,24 @@
+/**
+ * tast.manager
+ *
+ * Used to get the highest priority task.
+ */
+
+module.exports = {
+
+  dispatch: function (creep) {
+    // TODO assign the most important task.
+  },
+
+  getWorkers: function () {
+    return _.filter(Game.creeps, (creep) => creep.memory.role == 'worker');
+  },
+
+  /**
+   * @param task the name of the task
+   * @return array of workers on that task
+   */
+  getTaskWorkers: function (task) {
+    return _.filter(this.getWorkers, (creep) => creep.memory.task == task)
+  }
+};
